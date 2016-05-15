b91ad7d65214761419f633ce7118fcd9a68db28c
jdimac <judi0713@sina.com>
2016-05-15 22:48:15 +0800

    增加开机启动

--- Gank/AppDelegate.swift
+++ Gank/AppDelegate.swift
@@ -31,10 +31,19 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 		popover.animates = false
 		popover.appearance = NSAppearance(named: NSAppearanceNameAqua) // since os x 10.9
 
+        //增加开机启动
+        if !applicationIsInStartUpItems() {
+            toggleLaunchAtStartup()
+        }
+
+        
 		// 设置定时器每隔10分钟发送数据请求 10.minutes
 		refreshTimer = NSTimer.every(10.minutes) { // seconds
 			NSNotificationCenter.defaultCenter().postNotificationName("ReloadSections", object: nil)
 		}
+        
+        
+        
 	}
 
 	func applicationWillTerminate(aNotification: NSNotification) {
@@ -65,5 +74,69 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 		NSApplication.sharedApplication().terminate(self)
 	}
 
+    //增加开机启动 10.10
+    func applicationIsInStartUpItems() -> Bool {
+        return itemReferencesInLoginItems().existingReference != nil
+    }
+    
+    func toggleLaunchAtStartup() {
+        let itemReferences = itemReferencesInLoginItems()
+        let shouldBeToggled = (itemReferences.existingReference == nil)
+        let loginItemsRef = LSSharedFileListCreate(
+            nil,
+            kLSSharedFileListSessionLoginItems.takeRetainedValue(),
+            nil
+            ).takeRetainedValue() as LSSharedFileListRef?
+        
+        if loginItemsRef != nil {
+            if shouldBeToggled {
+                if let appUrl: CFURLRef = NSURL.fileURLWithPath(NSBundle.mainBundle().bundlePath) {
+                    LSSharedFileListInsertItemURL(loginItemsRef, itemReferences.lastReference, nil, nil, appUrl, nil, nil)
+                }
+            } else {
+                if let itemRef = itemReferences.existingReference {
+                    LSSharedFileListItemRemove(loginItemsRef,itemRef);
+                }
+            }
+        }
+    }
+    
+    func itemReferencesInLoginItems() -> (existingReference: LSSharedFileListItemRef?, lastReference: LSSharedFileListItemRef?) {
+        let  itemUrl : UnsafeMutablePointer<Unmanaged<CFURL>?> = UnsafeMutablePointer<Unmanaged<CFURL>?>.alloc(1)
+        if let appUrl : NSURL = NSURL.fileURLWithPath(NSBundle.mainBundle().bundlePath) {
+            let loginItemsRef = LSSharedFileListCreate(
+                nil,
+                kLSSharedFileListSessionLoginItems.takeRetainedValue(),
+                nil
+                ).takeRetainedValue() as LSSharedFileListRef?
+            
+            if loginItemsRef != nil {
+                let loginItems = LSSharedFileListCopySnapshot(loginItemsRef, nil).takeRetainedValue() as NSArray
+                if(loginItems.count > 0) {
+                    let lastItemRef = loginItems.lastObject as! LSSharedFileListItemRef
+                    
+                    for i in 0 ..< loginItems.count {
+                        let currentItemRef = loginItems.objectAtIndex(i) as! LSSharedFileListItemRef
+                        
+                        if LSSharedFileListItemResolve(currentItemRef, 0, itemUrl, nil) == noErr {
+                            if let urlRef: NSURL = itemUrl.memory?.takeRetainedValue() {
+                                if urlRef.isEqual(appUrl) {
+                                    return (currentItemRef, lastItemRef)
+                                }
+                            }
+                        }
+                    }
+                    // The application was not found in the startup list
+                    return (nil, lastItemRef)
+                    
+                } else  {
+                    let addatstart: LSSharedFileListItemRef = kLSSharedFileListItemBeforeFirst.takeRetainedValue()
+                    return(nil,addatstart)
+                }
+            }
+        }
+        
+        return (nil, nil)
+    }
 }
 
